bdec43494370fe53bb8f424a1d2d0d1c8a4ab8a2
Jan Acuna <jan.acuna565@gmail.com>
2020-08-13 12:50:39 -0500

    feat(index): Add env file

--- index.js
+++ index.js
@@ -5,25 +5,30 @@ let app = express();
 
 let apiRoutes = require("./api-routes");
 
+require('dotenv').config({path: '.env'});
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/zoetic', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 var db = mongoose.connection;
 
 (!db) ? console.log("Error connecting db") : console.log("Db connected successfully")
 
-var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send('NodeJs + Express + mongo server :)'));
 
 app.use('/api', apiRoutes);
 
 // Launch app to listen to specified port
-app.listen(port, function () {
-    console.log("Running ApiREST on port " + port);
+
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 8080;
+
+app.listen(port, host, () =>{
+    console.log("Running ApiREST on port " + port); 
 });
